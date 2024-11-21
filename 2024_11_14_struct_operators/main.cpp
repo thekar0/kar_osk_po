a1098a48cb0ede20d4762e5096084b216761ee1d
oskark1c <oskark1c@DOM.SCI.EDU.PL>
2024-11-21 15:32:23 +0100

    struct operators

---
+++ 2024_11_14_struct_operators/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ostream>
+
+struct Object
+{
+    int a;
+    std::string op;
+    int b;
+    float result;
+
+    friend std::ostream& operator<<(std::ostream& os, const Object& obj)
+    {
+        os << obj.a << " " << obj.op << " " << obj.b << " = " << obj.result << "\n";
+        return os;
+    }
+};
+
+int main()
+{
+    std::ofstream file("data.txt");
+
+    std::string operators[5] = {"addition", "subtraction", "multiplication", "division", "modulo"};
+
+    for(int i = 0; i < 10; i++)
+    {
+        Object object;
+        object.a = rand() % 100;
+        object.op = operators[rand() % 5];
+        object.b = rand() % 100;
+        
+        if(object.op == "addition")
+        {
+            object.result = object.a + object.b;
+        }
+        else if(object.op == "subtraction")
+        {
+            object.result = object.a - object.b;
+        }
+        else if(object.op == "multiplication")
+        {
+            object.result = object.a * object.b;
+        }
+        else if(object.op == "division")
+        {
+            if (object.b != 0)
+            {
+                object.result = object.a / object.b;
+            }
+            else
+            {
+                object.result = 0;
+            }
+        }
+        else if(object.op == "modulo")
+        {
+            if (object.b != 0)
+            {
+                object.result = object.a % object.b;
+            }
+            else
+            {
+                object.result = 0;
+            }
+        }
+
+        file << object;
+    }
+
+    file.close();
+    return 0;
+}
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/kar0/Documents/kar_osk_po
+# Install script for directory: C:/Users/oskark1c/Desktop/kar_osk_po
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/sci_po")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -27,29 +27,24 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/oskark1c/Desktop/kar_osk_po/build/2024_09_24_funkcje_matematyczne/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/kar0/Documents/kar_osk_po/build/2024_09_24_funkcje_matematyczne/cmake_install.cmake")
+  include("C:/Users/oskark1c/Desktop/kar_osk_po/build/2024_10_25_generator_solver/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/kar0/Documents/kar_osk_po/build/2024_10_25_generator_solver/cmake_install.cmake")
+  include("C:/Users/oskark1c/Desktop/kar_osk_po/build/2024_11_14_struct_operators/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -60,5 +55,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/kar0/Documents/kar_osk_po/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/oskark1c/Desktop/kar_osk_po/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
